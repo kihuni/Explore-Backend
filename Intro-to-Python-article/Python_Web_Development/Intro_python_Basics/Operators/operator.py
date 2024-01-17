285a4a43d962fe268df137dbf4802e0796abfa21
kihuni <stephenkihuni55@gmail.com>
2024-01-17 12:38:45 +0300

    added operators

---
+++ Intro-to-Python-article/Python_Web_Development/Intro_python_Basics/Operators/operator.py
@@ -0,0 +1,54 @@
+# 1 Arithmetic operators
+x = 10
+y = 3
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+modulus = x % y
+exponentiation = x ** y
+floor_division = x // y
+
+# 2 Comparison operators
+a = 5
+b = 7
+
+equal = a == b
+not_equal = a != b
+greater_than = a > b
+less_than = a < b
+greater_than_or_equal = a >= b
+less_than_or_equal = a <= b
+
+# 3  Logical operators
+p = True
+q = False
+
+logical_and = p and q
+logical_or = p or q
+logical_not = not p
+
+# 4 Assignment operators
+num = 10
+
+num += 5  # equivalent to num = num + 5
+num -= 3  # equivalent to num = num - 3
+num *= 2  # equivalent to num = num * 2
+num /= 4  # equivalent to num = num / 4
+num %= 3  # equivalent to num = num % 3
+num //= 2  # equivalent to num = num // 2
+num **= 3  # equivalent to num = num ** 3
+
+# 5 Membership operators
+list1 = [1, 2, 3, 4, 5]
+
+is_member = 3 in list1
+is_not_member = 6 not in list1
+
+# 6 Identity operators
+x = 10
+y = 10
+
+is_same = x is y
+is_not_same = x is not y
